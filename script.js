98444dc9bd0d789bc77cd22caf491b8e37233275
ProfRicardoMaciel <maciel.ricardo@escola.pr.gov.br>
2025-04-09 19:44:27 -0300

    .

--- script.js
+++ script.js
@@ -12,11 +12,6 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
 });
-     diminuiFonteBotao.addEventListener('click', function(){
-         tamanhoAtualFonte -= 0.1;
-         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
- 
-     })
 
     
      alternaContraste.addEventListener('click', function(){
